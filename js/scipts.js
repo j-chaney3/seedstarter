afb391891eb13e47ec772de9fef819db0aa440d1
Jame Chaney <macneilslt@gmail.com>
2023-04-06 22:07:22 -0400

    Added more seeds

--- js/scipts.js
+++ js/scipts.js
@@ -1,29 +1,27 @@
 class Seed {
-    constructor(type, season, start) {
-        this.type = type;
-        this.season = season;
-        this.start = start;
-    }
+  constructor(type, season, start) {
+    this.type = type;
+    this.season = season;
+    this.start = start;
+  }
 
-    plantMe() {
+  plantMe() {
+    let div = document.getElementById(this.season);
 
+    let b = document.createElement("button");
+    let br = document.createElement("br");
+    b.setAttribute("class", "btn btn-success btn-sm mt-1");
+    b.setAttribute("data-html", "true");
+    b.setAttribute("data-toggle", "tooltip");
+    b.setAttribute("data-placement", "right");
+    b.setAttribute("title", `${this.type} <br> ${this.start}`);
 
-
-        let div = document.getElementById(this.season);
-        
-        let b = document.createElement('button');
-        let br = document.createElement('br');
-        b.setAttribute('class', 'btn btn-success btn-sm mt-1');
-        b.setAttribute('data-html', 'true');
-        b.setAttribute('data-toggle', 'tooltip');
-        b.setAttribute('data-placement', 'right');
-        b.setAttribute('title', `${this.type} <br> ${this.start}`);
-    
-        b.textContent = this.type;
-        div.appendChild(b);
-        div.appendChild(br);
-    }
+    b.textContent = this.type;
+    div.appendChild(b);
+    div.appendChild(br);
+  }
 }
+
 //spring
 const carrot = new Seed("carrot", "spring", "march 1st");
 carrot.plantMe();
@@ -37,14 +35,6 @@ const garlic = new Seed("Garlic", "fall", "September 1st");
 garlic.plantMe();
 //winter
 
-
-
-
-
-
-
-
 $(function () {
-    $('[data-toggle="tooltip"]').tooltip()
-})
-
+  $('[data-toggle="tooltip"]').tooltip();
+});
